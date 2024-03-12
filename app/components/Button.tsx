46778b767423873a57331b602deda6ebe1caba02
Adil <yakdi.adil@gmail.com>
2024-03-12 17:25:55 +0100

    🎨 button style

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,11 +1,20 @@
 import { cva } from "class-variance-authority";
 import React from "react";
 
-const buttonStyles = cva(["hover:bg-secondary-hover", "transition-colors"], {
+const buttonStyles = cva(["transition-colors"], {
 	variants: {
 		size: {
-			default: [],
-			icon: [],
+			default: ["bg-secondary", "hover:bg-secondary-hover"],
+			ghost: ["hover:bg-gray-100"],
+			icon: [
+				"roudend-full",
+				"w-10",
+				"h-10",
+				"flex",
+				"items-center",
+				"justify-center",
+				"p-2.5",
+			],
 		},
 	},
 });
